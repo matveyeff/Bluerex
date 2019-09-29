8c97d719682a5d4ea0a0231b9159fd4843cfe814
Dmitrii Matveev <matveyeff@mail.ru>
2019-09-29 19:21:37 +0300

    added scroll to top button

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,19 @@ $(function(){
 
 	baguetteBox.run('.gallery');
 
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 300) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
 });
 
 
