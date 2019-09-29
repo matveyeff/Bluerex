0d38c7e916676536c91f580bafe3ac2d396ade22
Dmitrii Matveev <matveyeff@mail.ru>
2019-09-29 19:55:51 +0300

    added preloader

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 $(function(){
 
+	$(window).on('load', function(){
+		$('.preloader').delay(500).fadeOut('slow', function(){
+			$(this).attr('style', 'display: none !important');
+		});
+	});
+
 	baguetteBox.run('.gallery');
 
 	$(window).scroll(function(){
